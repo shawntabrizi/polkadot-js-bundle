fd120bd3950fdebcc67b40caa312b072a5f15225
Shawn Tabrizi <shawntabrizi@gmail.com>
2020-05-26 22:11:59 +0200

    API version 1.16.1

---
+++ index.js
@@ -0,0 +1,9 @@
+ let api = require("@polkadot/api");
+ let util = require("@polkadot/util");
+ let util_crypto = require("@polkadot/util-crypto");
+ let keyring = require("@polkadot/keyring");
+
+ window.api = api;
+ window.util = util;
+ window.util_crypto = util_crypto;
+ window.keyring = keyring;
