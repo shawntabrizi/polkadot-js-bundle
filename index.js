98ec43ab0280653b76c31da2724e572d6a6cbf3a
Shawn Tabrizi <shawntabrizi@gmail.com>
2020-07-28 13:36:55 +0200

    add polkadot types

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
- let api = require("@polkadot/api");
- let util = require("@polkadot/util");
- let util_crypto = require("@polkadot/util-crypto");
- let keyring = require("@polkadot/keyring");
+let api = require("@polkadot/api");
+let util = require("@polkadot/util");
+let util_crypto = require("@polkadot/util-crypto");
+let keyring = require("@polkadot/keyring");
+let types = require("@polkadot/types")
 
- window.api = api;
- window.util = util;
- window.util_crypto = util_crypto;
- window.keyring = keyring;
+window.api = api;
+window.util = util;
+window.util_crypto = util_crypto;
+window.keyring = keyring;
+window.types = types;
